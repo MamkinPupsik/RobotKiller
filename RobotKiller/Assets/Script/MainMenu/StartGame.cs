8464811893768030d45ff023119cb8e031ccf86a
Dimidroll <JiJay.mr@mail.ru>
2023-02-08 20:02:40 +0600

    Add platform for main menu

---
+++ RobotKiller/Assets/Script/MainMenu/StartGame.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    //Скрипт при нажатии на Start game запускает игру
+
+}
---
+++ RobotKiller/Assets/Script/MainMenu/StartSettings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSettings : MonoBehaviour
+{
+
+    // При запуске на кнопку settings запускает настройки 
+
+}
