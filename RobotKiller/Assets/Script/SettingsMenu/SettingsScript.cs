e5422cc5d54c2e426f84e8b5470d0fc1e2b784df
Dimidroll <JiJay.mr@mail.ru>
2023-02-12 21:45:08 +0600

    Change Visual window, add settings menu(quality, resolution)

---
+++ RobotKiller/Assets/Script/SettingsMenu/SettingsScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour
+{
+    public Dropdown resolutionDropdown;
+    public Dropdown qualityDropDown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;               
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("QualitySettingsPreference", qualityDropDown.value);
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPreference", System.Convert.ToInt32(Screen.fullScreen));
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("QualitySettingsPreference"))
+            qualityDropDown.value = PlayerPrefs.GetInt("QualitySettingsPreference");
+        else
+            qualityDropDown.value = 3;
+
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            resolutionDropdown.value = currentResolutionIndex;
+
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen = System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
+    }
+}
